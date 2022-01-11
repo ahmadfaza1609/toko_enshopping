43741d5e97d88bcc5245135a20c17123d79ee0ea
Ahmad Faza Hazmi <61263298+ahmadfaza1609@users.noreply.github.com>
2022-01-11 21:05:27 +0700

    database product bigInteger change string

--- database/migrations/2022_01_08_013914_create_product_table.php
+++ database/migrations/2022_01_08_013914_create_product_table.php
@@ -23,7 +23,7 @@ class CreateProductTable extends Migration
             $table->string('link_shoope')->nullable();
             $table->string('link_tokped')->nullable();
             $table->string('link_lazada')->nullable();
-            $table->bigInteger('category_id');
+            $table->string('category_id');
             $table->softDeletes();
             $table->timestamps();
         });
