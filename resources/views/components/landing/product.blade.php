3ad5ca8f36b9ce4f3eb8a8a8357175dbeef945ea
Ahmad Faza Hazmi <61263298+ahmadfaza1609@users.noreply.github.com>
2022-01-15 15:56:52 +0700

    img product

--- resources/views/components/landing/product.blade.php
+++ resources/views/components/landing/product.blade.php
@@ -3,7 +3,7 @@
         @foreach($product as $p)
         <a href="{{ route('detail.landing', $p->id) }}" class="text-decoration-none __body-card">
             <div class="card shadow rounded __box-product ">
-                <img src="{{ url('https://via.placeholder.com/750x500') }}" alt="placeholder" class="card-img-top" />
+                <img src="{{ url('https://via.placeholder.com/750x500') }}" alt="placeholder" class="card-img-top" width="100%" height="225"/>
 
                 <div class="card-body">
                     <h1 class="card-title fs-5 fw-bold">{{ $p->title}}</h1>
@@ -11,7 +11,6 @@
                     <div class="d-flex justify-content-between align-items-center pt-3">
 
                     <a href="#" class="btn px-4 me-auto btn-sm btn-success">Beli</a>
-                    <small class="text-muted fst-italic">Shopee</small>
                     </div>
                 </div>
             </div>
